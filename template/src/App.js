d6ddf5c0d7543cd17ba45d4af495d7a7e4b1f772
ducnm <ducnm@ducnms-MacBook-Pro.local>
2018-08-25 10:00:23 +0700

    update code

--- template/src/App.js
+++ template/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component, createContext } from 'react';
 
+
+
+
+//context api
 // const AppContext = createContext()
 // class App extends Component {
 //   render() {
@@ -24,6 +28,7 @@ import React, { Component, createContext } from 'react';
 //     }} />
 //         <div>------------------------------ children2 <Children2 /></div>
 //     </AppContext.Provider>
+
 
 //     );
 //   }
@@ -50,81 +55,137 @@ import React, { Component, createContext } from 'react';
 //     );
 //   }
 // }
-import { Provider, Subscribe, Container } from 'unstated';
-class AppContainer extends Container {
-  state = { value: '' };
-}
 
-class App extends Component {
+
+
+
+
+
+// // unstate
+// import { Provider, Subscribe, Container } from 'unstated';
+// class AppContainer extends Container {
+//   state = { value: '' };
+// }
+
+// class App extends Component {
+//   render() {
+//     return (<Provider>
+//     <Subscribe to={[AppContainer]}>
+//         {
+//           container => {
+//             return    <div> ------------------------------------------- Children1  {container.state.value} <Children1 /></div>
+//           }
+//         }
+//     </Subscribe>
+
+//     </Provider>
+//     );
+//   }
+// }
+// class Children1 extends Component {
+
+//   render() {
+//     return (<div>------------------------------------ children2 <Children2 /></div>)
+//   }
+// }
+// class Children2 extends Component {
+//   render() {
+//     return (
+//       <div>--------------------------------children 3<Children3 /></div>
+
+//     );
+//   }
+// }
+// class Children3 extends Component {
+//   render() {
+//     return (
+//       <Subscribe to={[AppContainer]}>
+//         {
+//           container => {
+//             return (<div> -------------- Value to component App : <input onChange={(e) => {
+//               container.setState({ value: e.target.value })
+//             }} />  
+//             <Children4 /></div>)
+//           }
+//         }
+//       </Subscribe>
+
+
+//     );
+//   }
+// }
+// class Children4 extends Component {
+//   render() {
+//     return (
+//       <Subscribe to={[AppContainer]}>
+//         {
+//           container => {
+//             return (<div> -----------------Children 4 : {container.state.value} </div>)
+//           }
+//         }
+//       </Subscribe>
+
+
+//     );
+//   }
+// }
+// export default App;
+
+
+//combine unstate  and context
+import { Provider, Subscribe, Container } from 'unstated';
+
+const CombineContext = createContext()
+class CombineContainer1 extends Container {
+  state = { value: 'CombineContainer1' };
+}
+class CombineContainer2 extends Container {
+  state = { value: 'CombineContainer2' };
+}
+class CombineContextUnsate extends Component {
   render() {
-    return (
-      <Provider>
-        <Subscribe to={[AppContainer]}>
-          {container => {
-            return (
-              <div>
-                {' '}
-                ------------------------------------------- Children1{' '}
-                {container.state.value} <Children1 />
-              </div>
-            );
-          }}
-        </Subscribe>
-      </Provider>
-    );
+    return <Provider>
+      <CombineContext.Provider value={CombineContainer2}>
+     ----------------------------------------- Children1 <Children1 />
+    </CombineContext.Provider>
+    </Provider>
+    
   }
 }
 class Children1 extends Component {
   render() {
     return (
-      <div>
-        ------------------------------------ children2 <Children2 />
-      </div>
-    );
+     <div>
+       -------------------------------------Children2<Children2 />
+     </div>
+    )
   }
 }
 class Children2 extends Component {
   render() {
-    return (
-      <div>
-        --------------------------------children 3<Children3 />
-      </div>
-    );
+    return (<div>---------------------Children3<Children3 /></div>
+
+    )
   }
 }
+
 class Children3 extends Component {
   render() {
-    return (
-      <Subscribe to={[AppContainer]}>
-        {container => {
-          return (
-            <div>
-              {' '}
-              -------------- Value to component App :{' '}
-              <input
-                onChange={e => {
-                  container.setState({ value: e.target.value });
-                }}
-              />
-              <Children4 />
-            </div>
-          );
-        }}
-      </Subscribe>
-    );
+    return (<CombineContext.Consumer>
+      {
+        containerContext => {
+          return <Subscribe to={[containerContext]}>
+            {
+              container => {
+                return <div> --------------- <b>Value to container:</b> {container.state.value}</div>
+              }
+            }
+          </Subscribe>
+        }
+      }
+    </CombineContext.Consumer>
+    )
   }
 }
-class Children4 extends Component {
-  render() {
-    return (
-      <Subscribe to={[AppContainer]}>
-        {container => {
-          return (
-            <div> -----------------Children 4 : {container.state.value} </div>
-          );
-        }}
-      </Subscribe>
-    );
-  }
-}
-export default App;
+
+export default CombineContextUnsate;
